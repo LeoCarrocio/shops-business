bcf1728d07752606e11f23d907c35614dc2c9995
leocarrocio <lcarrocio-at-424247187151>
2023-01-25 22:41:56 -0300

    search for product whit param url

--- database/dbProducts.ts
+++ database/dbProducts.ts
@@ -20,11 +20,29 @@ interface ProductSlugs {
   slug:string
 }
 
-export const getAllProductSlugs = async(): Promise<ProductSlugs[]> =>{
+export const getAllProductSlugs = async(): Promise<ProductSlugs[]> => {
 
   await db.connect();
   const slugs = await Product.find().select('slug - _id').lean();
   await db.disconnect();
 
   return slugs;
+}
+
+export const getProductByTerm = async (term: string): Promise<IProduct[]> => {
+
+  term = term.toString().toLowerCase();
+
+  await db.connect();
+
+  const product = await Product.find({
+    $text : { $search: term }
+  })
+  .select('title images price inStock slug -id')
+  .lean();
+
+  await db.disconnect()
+
+  return product;
+
 }
--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetServerSideProps } from 'next'
 import { ShopLayout } from '../../components/layout';
 import { ProductList } from '../../components/product/ProductList';
 import {useProducts} from '../../hooks'
@@ -9,12 +9,20 @@ import { initialData } from '../database/products';
 */
 
 import { Typography} from '@mui/material';
+import Product from '../../model/Product';
+import { dbProducts } from '../../database';
+import { IProduct } from '../../interfaces';
 
 
-const SearchHomePage: NextPage = () => {
+interface PropsSearchHomePage{
+  products: IProduct[]
+}
+
+
+const SearchHomePage: NextPage<PropsSearchHomePage> = ({products}) => {
 
   // podriamos utilizar esto y funcinaria muy bien ya que saco la busqieda por la direccion pero lo bamos hacer mejor con la kukies, (video 229)
-  const {products, isLoading, isError} = useProducts('/search/camisetas'); 
+  // const {products, isLoading, isError} = useProducts('/search/camisetas'); 
 
   return (
     <ShopLayout title={'Home - Search '} pageDescription={'Busqueda de producto '}>
@@ -22,11 +30,39 @@ const SearchHomePage: NextPage = () => {
       <Typography variant='h2' sx={{ mb:'1'}} > Todos los productos Buscado.... </Typography>
 
       {
-        isLoading ? <FullScreemLoading /> :  <ProductList products={ products }/>
+        // como nonutilizo la busqueda por params ya no nesesito esto 
+        // isLoading ? <FullScreemLoading /> :  <ProductList products={ products }/>
       }
+
+      <ProductList products={ products }/>
 
     </ShopLayout>
   )
 }
+
+
+export const getServerSideProps:GetServerSideProps = async ({params}) => {
+
+  const { query = '' } = params as { query:string };
+
+  if( query.length === 0 ){
+    return {
+      redirect :{
+        destination:'/',
+        permanent: true 
+      }
+    }
+  }
+// como estoy del lado del servidor no utilizo mi api sino una funcion de db
+  let product = await dbProducts.getProductByTerm(query)
+
+  return{
+    props:{
+      product
+    } 
+  }
+}
+
+
 
 export default SearchHomePage;
